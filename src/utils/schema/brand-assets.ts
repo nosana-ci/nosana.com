480ea91bb3cc3602763febe56e27e021e28e87c5
Dharmesh <dharmeshwr@gmail.com>
2026-02-17 21:07:42 +0530

    feat: add brand asset page

--- src/utils/schema/brand-assets.ts
+++ src/utils/schema/brand-assets.ts
@@ -1,3 +1,44 @@
 import { z, type SchemaContext } from "astro:content";
 
-export const brandAssetsSchema = ({ image }: SchemaContext) => z.object({});
+export const brandAssetsSchema = ({ image }: SchemaContext) =>
+  z.object({
+    heroSection: z.object({
+      badge: z.string(),
+      heading: z.string(),
+      description: z.string(),
+      downloadUrl: z.string(),
+    }),
+    logoSection: z.object({
+      heading: z.string(),
+      description: z.string(),
+      downloadUrl: z.string(),
+    }),
+    typographySection: z.object({
+      heading: z.string(),
+      description: z.string(),
+      typefaces: z.array(
+        z.object({
+          name: z.string(),
+          weights: z.array(z.string()),
+        }),
+      ),
+    }),
+    colorsSection: z.object({
+      heading: z.string(),
+      description: z.string(),
+      brandColors: z.array(
+        z.object({
+          name: z.string(),
+          hex: z.string(),
+          textDark: z.boolean().optional(),
+        }),
+      ),
+      neutralColors: z.array(
+        z.object({
+          name: z.string(),
+          hex: z.string(),
+          textDark: z.boolean().optional(),
+        }),
+      ),
+    }),
+  });
