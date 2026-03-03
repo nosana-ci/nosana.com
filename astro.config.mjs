0b74ccd100b0de75f76a7387cc00573aa3b343cd
MaybeTarun <tarunthegreat2003@gmail.com>
2026-03-03 19:01:20 +0530

    responsive issues fix, added newsletter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nosana-new.vercel.app',
+  output: "server",
+  adapter: vercel(),
   server: {
     host: true,
   },
